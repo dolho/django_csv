d6221da1315adb72faf8f94b3ad5d01c2dca5e83
Oleksii Dolhov <oleksii.dolhov@gmail.com>
2022-03-30 21:00:46 +0300

    remaking login

--- accounts/views.py
+++ accounts/views.py
@@ -18,20 +18,20 @@ class SignUpView(CreateView):
 
 
 #
-# def login_view(request):
-#     if request.method == "POST":
-#         # Attempt to sign user in
-#         username = request.POST["username"]
-#         password = request.POST["password"]
-#         user = authenticate(request, username=username, password=password)
-#
-#         # Check if authentication successful
-#         if user is not None:
-#             login(request, user)
-#             return HttpResponseRedirect(reverse('index'))
-#         else:
-#             return render(request, 'registration/login.html', {
-#                 "message": "Invalid username and/or password."
-#             })
-#     else:
-#         return render(request, "registration/login.html", {"login_url": reverse('login')})
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'registration/login.html', {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "registration/login.html", {"login_url": reverse('login')})
